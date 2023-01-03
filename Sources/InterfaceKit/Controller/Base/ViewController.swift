94d8560c00dfca4b7c5668fa1fd1609298e006f0
Ed <pselok@gmail.com>
2023-01-03 19:47:54 +0400

    wip

--- Sources/InterfaceKit/Controller/Base/ViewController.swift
+++ Sources/InterfaceKit/Controller/Base/ViewController.swift
@@ -45,11 +45,17 @@ extension ViewController {
     }
 }
 extension ViewController {
+    public var hasNavBar: Bool {
+        return !navBarItems.empty || navigation?.rootViewController != self
+    }
     public var navBarOffsets: Bool {
         return true
     }
-    public var hasNavBar: Bool {
-        return !navBarItems.empty || navigation?.rootViewController != self
+    public var presented: ViewController? {
+        return presentedViewController as? ViewController
+    }
+    public var presenting: ViewController? {
+        return presentingViewController as? ViewController
     }
 }
 extension Container {
