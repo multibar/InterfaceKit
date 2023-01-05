892051c39a52bee0ec91deb8ec2567d6e52f77a6
Ed <pselok@gmail.com>
2023-01-06 02:44:45 +0400

    inset all

--- Sources/InterfaceKit/Interface/Extensions/Insets.swift
+++ Sources/InterfaceKit/Interface/Extensions/Insets.swift
@@ -1,6 +1,9 @@
 import UIKit
 
 extension UIEdgeInsets {
+    public static func insets(all inset: CGFloat) -> UIEdgeInsets {
+        return UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
+    }
     public static func insets(top: CGFloat, left: CGFloat, right: CGFloat, bottom: CGFloat) -> UIEdgeInsets {
         return UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
     }
